9dc7bc7e3777c0b82e1029e8a134a5932c59385c
svetlana-kuprina <kupsveta@rambler.ru>
2023-05-01 15:47:28 +0500

    add function search,remove,edit

--- index.php
+++ index.php
@@ -10,18 +10,39 @@ function add(&$textStorage,string $title,string $text) : void
 
 }
 
-//function search($textStorage)
-//{
-//for ($i=0,i++)
-//{
-//    array_search ('Dell',$textStorage[$i]);
-//}
-//}
-
+function search($textStorage)
+{
+foreach ($textStorage as $key =>$value)
+{
+if(in_array('Dell',$value)){
+    print_r("Ключ".$key);
+    return($key);
+}
+}
+}
 
 function remove(&$textStorage)
 {
+    $key=search($textStorage);
+    var_dump($key);
+    if ($key!==NULL)
+    {
+        unset($textStorage[$key]);
+        return true;}
+    else
+        return false;
 
+}
+
+function edit (&$textStorage,int $index,string $titleArr,string $textArr)
+{
+       if (array_key_exists ($index,$textStorage))
+    {
+        $textStorage[$index][$titleArr]=$textArr;
+        return true;
+    }
+    else
+        return false;
 }
 
 $i=0;
@@ -35,5 +56,7 @@ do{
 } while($i<2);
 
 print_r($textStorage);
-search ($textStorage);
-
+var_dump(remove($textStorage));
+print_r($textStorage);
+var_dump(edit($textStorage,5,'title','Привет'));
+print_r($textStorage);
